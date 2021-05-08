1a48d3f766028f0c9e7c9abb1a016178c2918702
Victor <kyoouth@gmail.com>
2021-05-08 13:09:14 -0300

    Nav modal style done

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,42 @@
 import "./Nav.scss"
 import logo from "../../assets/images/logo.svg"
+import { useState } from 'react'
+
 
 
 function Nav() {
-    return (
-        <nav className="nav">
-            <div className="nav__logo__container">
-                <img alt="shortly" src={logo} className="nav__logo" />
-            </div>
-            <div className="nav__menu">
-                <i className="fas fa-bars nav__menu__icon" onClick={() => { alert("teste") }}></i>
-            </div>
 
-        </nav>
+    const [modalDisplay, setModalDisplay] = useState("none")
+
+    const handleMenu = () => {
+        modalDisplay === "none"
+            ? setModalDisplay("flex")
+            : setModalDisplay("none")
+    }
+
+    return (
+        <div className="nav__container">
+            <nav className="nav">
+                <div className="nav__logo__container">
+                    <img alt="shortly" src={logo} className="nav__logo" />
+                </div>
+                <div className="nav__menu">
+                    <i className="fas fa-bars nav__menu__icon" onClick={handleMenu}></i>
+                </div>
+            </nav>
+            <div className="modal__container" style={{ display: modalDisplay }}>
+                <div className="modal">
+                    <a href="https://google.com">Features</a>
+                    <a href="https://google.com">Pricing</a>
+                    <a href="https://google.com">Resources</a>
+                    <hr />
+                    <a href="https://google.com">Login</a>
+                    <a href="https://google.com">Sign Up</a>
+
+
+                </div>
+            </div>
+        </div>
     )
 }
 
