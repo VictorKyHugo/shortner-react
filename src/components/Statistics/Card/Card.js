4d792ded681e3b1f94fba49dd73c7c8f0ff42be5
Victor <kyoouth@gmail.com>
2021-05-15 16:58:09 -0300

    card template done

--- src/components/Statistics/Card/Card.js
+++ src/components/Statistics/Card/Card.js
@@ -1,7 +1,16 @@
-function Card() {
-    return (
-        <div>
+import './Card.scss'
+import icon from '../../../assets/images/icon-detailed-records.svg'
 
+function Card(props) {
+    return (
+        <div className="card">
+            <div className="card__icon">
+                <img src={icon} alt="icon" />
+            </div>
+            <h3 className="card__title">Brand recognition</h3>
+            <p className="card__text"> Boost your brand recognition with each click. Generic links don’t
+            mean a thing. Branded links help instil confidence in your content.
+</p>
         </div>
     )
 }
