8f00ea84451f382117e75e75264b032fe93618bf
Victor <kyoouth@gmail.com>
2021-06-02 08:21:38 -0300

    copy function done

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,13 +1,27 @@
 import './Link.scss'
+import { useState } from 'react'
 
 function Link(props) {
+
+    const [isCopied, setIsCopied] = useState(false)
+
+    const copyText = async () => {
+        const text = await navigator.clipboard.writeText(props.short)
+        setIsCopied(true)
+        return text
+    }
 
     return (
         <div className="link">
             <span className="link__complete">{props.original}</span>
             <div className="link__line"></div>
             <span className="link__short">{props.short}</span>
-            <button className="link__button" >Copy</button>
+            {
+                isCopied
+                    ? <button className="link__button link__button--copied">Copied!</button>
+                    : <button className="link__button" onClick={copyText} >Copy</button>
+            }
+
         </div>
     )
 }
